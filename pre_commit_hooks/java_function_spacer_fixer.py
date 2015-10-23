74c7d502e0588bb48c3257f1438d2d6485c59325
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-10-23 18:10:06 -0400

    added test prehook

---
+++ pre_commit_hooks/java_function_spacer_fixer.py
@@ -0,0 +1,11 @@
+import argparse
+import fileinput
+import os
+import sys
+
+
+def fix_trailing_whitespace(argv=None):
+	print("Eggs");
+
+if __name__ == '__main__':
+    sys.exit(fix_trailing_whitespace())
