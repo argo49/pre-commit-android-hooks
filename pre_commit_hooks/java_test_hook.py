37480c85ee66d09f8d43042092b7fc7c988a962b
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-10-23 22:50:48 -0400

    updated test

--- pre_commit_hooks/java_test_hook.py
+++ pre_commit_hooks/java_test_hook.py
@@ -5,8 +5,5 @@ import sys
 
 
 def fix_trailing_whitespace(argv=None):
-	print("Eggs");
-	return 1;
-
-if __name__ == '__main__':
-    sys.exit(fix_trailing_whitespace())
+	retv = 0
+    return retv
