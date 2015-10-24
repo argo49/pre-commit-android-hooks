4944897689ae1a2a45ea0bcd236de287f17b5fc0
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-10-24 15:52:15 -0400

    added back function 2

---
+++ pre_commit_hooks/java_function_spacer_fixer_two.py
@@ -0,0 +1,5 @@
+
+
+def fix_spaces_between_java_functions_two(argv=None):
+    retv = 0
+    return retv
--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     entry_points={
         'console_scripts': [
             'java-function-spacer-fixer = pre_commit_hooks.java_function_spacer_fixer:fix_spaces_between_java_functions',
+			'java-function-spacer-fixer-two = pre_commit_hooks.java_function_spacer_fixer_two:fix_spaces_between_java_functions_two',
 		],
     },
 )
