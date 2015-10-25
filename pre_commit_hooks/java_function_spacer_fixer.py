8608604d77d13755399de34a92422d73a729c2ce
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-10-24 21:36:52 -0400

    updated function spacer fixer to output file name given

--- pre_commit_hooks/java_function_spacer_fixer.py
+++ pre_commit_hooks/java_function_spacer_fixer.py
@@ -1,5 +1,18 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
+import argparse
+import io
+import tokenize
 
 def fix_spaces_between_java_functions(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
     retv = 0
+
+    for filename in args.filenames:
+        print('Found file {0}'.format(filename))
     return retv
